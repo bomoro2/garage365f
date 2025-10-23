34b8b121070896a37a8a79367f06074dcd9a7bdb
Bonifacio Morales <bonifacio.morales18@gmail.com>
2025-10-22 23:17:18 -0300

    conexion a server en mac

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app.dart';
+import 'core/network/env.dart';
 
-void main() {
+
+void main() {    
+  // LOG: confirmar base url en consola
+  // ignore: avoid_print
+  print('[ENV] API_BASE_URL = ${AppEnv.baseUrl}');
   runApp(const ProviderScope(child: GarageApp()));
 }
